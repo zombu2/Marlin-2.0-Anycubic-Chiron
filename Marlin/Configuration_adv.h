cae873530056abaaa9b31fef9b76b79705730754
zombu2 <42103673+zombu2@users.noreply.github.com>
2019-08-29 21:34:56 -0400

    changed display update interval

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1094,7 +1094,7 @@
 
   // Enable this option and reduce the value to optimize screen updates.
   // The normal delay is 10µs. Use the lowest value that still gives a reliable display.
-  //#define DOGM_SPI_DELAY_US 5
+  #define DOGM_SPI_DELAY_US 5
 
   // Swap the CW/CCW indicators in the graphics overlay
   //#define OVERLAY_GFX_REVERSE
